5da5c13743d780bef63e5317d0542229ca0e0915
dzucconi <mail@damonzucconi.com>
2016-11-29 22:23:36 +0100

    Break up loading based on lines

--- app/javascripts/index.js
+++ app/javascripts/index.js
@@ -25,9 +25,12 @@ const DOM = dom([
 ]);
 
 const { message, min, max } = parameters({
-  message: 'We make a star as we make a constellation',
+  message: `We make a star
+  as we make a constellation
+  by putting its parts together
+  and marking off its boundaries`,
   min: 0,
-  max: 250,
+  max: 150,
 });
 
 const fetch = text => {
@@ -35,13 +38,18 @@ const fetch = text => {
   return axios.get(url);
 };
 
-export default () => {
-  fetch(states(message).join(' '))
-    .then(({ data: [{ suggestions }]}) => {
-      DOM.indicator.parentNode.removeChild(DOM.indicator);
+const suggest = text =>
+  fetch(states(text).join(' '))
+    .then(({ data: [{ suggestions }] }) => [text, suggestions]);
 
-      const playback = () =>
-        type(DOM.input, message, [min, max], aggregated => {
+export default () => {
+  const play = promises =>
+    promises.shift()
+      // Playback
+      .then(([text, suggestions]) => {
+        DOM.indicator.parentNode.removeChild(DOM.indicator);
+
+        return type(DOM.input, text, [min, max], aggregated => {
           const token = aggregated.split(' ').pop();
           const match = token.match(/(\w+)/g);
 
@@ -63,22 +71,32 @@ export default () => {
           DOM.keyboard.innerHTML = keyboard(letter.substr(-1));
 
           return Promise.resolve(true);
-        })
-
-        // Pause
-        .then(() => {
-          DOM.keyboard.innerHTML = keyboard();
-
-          return timeout(2500);
-        })
-
-        // Reset
-        .then(() => {
-          DOM.input.innerHTML = '';
-          playback();
         });
+      })
 
-      // Begin
-      playback();
-    });
+      // Reset
+      .then(() => {
+        if (promises.length) {
+          DOM.app.appendChild(DOM.indicator);
+          return play(promises);
+        }
+      });
+
+  const lines = message.split('\n').map(x => x.trim());
+
+  const init = () =>
+    lines.reduce((promise, line) => {
+      return promise.then(() => {
+        return play([suggest(line)])
+          .then(() => {
+            DOM.app.appendChild(DOM.indicator);
+          });
+      });
+    }, Promise.resolve(true))
+      .then(() => {
+        DOM.indicator.parentNode.removeChild(DOM.indicator);
+        DOM.keyboard.innerHTML = keyboard();
+      });
+
+  init();
 };
